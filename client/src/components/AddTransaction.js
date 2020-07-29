39290afa09912638218294e00ac99ed8f615ec10
arslan-aziz <arslan.aziz@outlook.com>
2020-07-28 21:28:35 -0400

    added internal state to forms for text inputs

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,45 +1,88 @@
 import React, { useState } from 'react';
 import '../App.css'
 
-const AddTransaction = () => {
+class AddTransaction extends React.Component {
 
-    return (
-        <div className='AddTransaction'>
-            <h4 style={{marginBottom:'5px'}}>Add Transaction</h4>
-            <form className='form'>
-                <label className='form-label'>Transaction Amount</label>
-                <input
-                    type='text'
-                    placeholder='Transaction Amount...'
-                    className='form-element'
-                />
-                <label className='form-label'>Description</label>
-                <input
-                    type='text'
-                    placeholder='Description'
-                    className='form-element'
-                />
-                <label className='form-label'>Category</label>
-                <input
-                    type='text'
-                    placeholder='Category'
-                    className='form-element'
-                />
-                <label className='form-label'>From/To Account</label>
-                <input
-                    type='text'
-                    placeholder='Account'
-                    className='form-element'
-                />
-                <input
-                    type='submit'
-                    value='Submit'
-                />
-            </form>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            amountText: '',
+            descriptionText: '',
+            categoryText: '',
+            accountText: ''
+        }
+    }
+
+    updateText = (e) => {
+        let update = {};
+        switch(e.target.id) {
+            case 'transactionInput':
+                update = {amountText: e.target.value};
+                break;
+            case 'descriptionInput':
+                update = {descriptionText: e.target.value};
+                break;
+            case 'categoryInput':
+                update = {categoryText: e.target.value};
+                break;
+            case 'accountInput':
+                update = {accountText: e.target.value};
+                break;
+            default:
+        }
+        this.setState(update);
+    }
+    
+
+    render() {
+        return (
+            <div className='AddTransaction'>
+                <h4 style={{marginBottom:'5px'}}>Add Transaction</h4>
+                <form className='form'>
+                    <label className='form-label'>Transaction Amount</label>
+                    <input
+                        type='text'
+                        placeholder='Transaction Amount...'
+                        className='form-element'
+                        value={this.state.amountText}
+                        id='transactionInput'
+                        onChange={this.updateText}
+                    />
+                    <label className='form-label'>Description</label>
+                    <input
+                        type='text'
+                        placeholder='Description'
+                        className='form-element'
+                        value={this.descriptionText}
+                        id='descriptionInput'
+                        onChange={this.updateText}
+                    />
+                    <label className='form-label'>Category</label>
+                    <input
+                        type='text'
+                        placeholder='Category'
+                        className='form-element'
+                        value={this.categoryText}
+                        id='categoryInput'
+                        onChange={this.updateText}
+                    />
+                    <label className='form-label'>From/To Account</label>
+                    <input
+                        type='text'
+                        placeholder='Account'
+                        className='form-element'
+                        value={this.accountText}
+                        id='accountInput'
+                        onChange={this.updateText}
+                    />
+                    <input
+                        type='submit'
+                        value='Submit'
+                    />
+                </form>
+            </div>
+        )
+    }
 }
-
-
 
 export default AddTransaction;
--- client/src/components/MoveFunds.js
+++ client/src/components/MoveFunds.js
@@ -1,35 +1,75 @@
 import React from 'react';
 
-const MoveFunds = (props) => {
-    return (
-        <div className='MoveFunds'>
-            <h4 style={{marginBottom:'5px'}}>Move Funds</h4>
-            <form className='form'>
-                <label className='form-label'>Move Amount</label>
-                <input
-                    type='text'
-                    placeholder='Transaction Amount...'
-                    className='form-element'
-                />
-                <label className='form-label'>From Account</label>
-                <input
-                    type='text'
-                    placeholder='From'
-                    className='form-element'
-                />
-                <label className='form-label'>To Account</label>
-                <input
-                    type='text'
-                    placeholder='To'
-                    className='form-element'
-                />
-                <input
-                    type='submit'
-                    value='Submit'
-                />
-            </form>
-    </div>        
-    )
+class MoveFunds extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            amountText: '',
+            fromText: '',
+            toText: ''
+        }
+    }
+
+    updateText = (e) => {
+        let result = {};
+        switch(e.target.id) {
+            case 'amountInput':
+                result = {amountText: e.target.value};
+                break;
+            case 'fromInput':
+                result = {fromText: e.target.value};
+                break;
+            case 'toInput':
+                result = {toText: e.target.value};
+                break;
+            default:
+        }
+        this.setState(result);
+    }
+
+    render() {
+        return (
+            <div className='MoveFunds'>
+                <h4 style={{marginBottom:'5px'}}>Move Funds</h4>
+                <form className='form'>
+                    <label className='form-label'>Move Amount</label>
+                    <input
+                        type='text'
+                        placeholder='Transaction Amount...'
+                        className='form-element'
+                        value={this.state.amountText}
+                        id='amountInput'
+                        onChange={this.updateText}
+                    />
+                    <label className='form-label'>From Account</label>
+                    <input
+                        type='text'
+                        placeholder='From'
+                        className='form-element'
+                        value={this.state.fromText}
+                        id='fromInput'
+                        onChange={this.updateText}
+                    />
+                    <label className='form-label'>To Account</label>
+                    <input
+                        type='text'
+                        placeholder='To'
+                        className='form-element'
+                        value={this.state.toText}
+                        id='toInput'
+                        onChange={this.updateText}
+                    />
+                    <input
+                        type='submit'
+                        value='Submit'
+                    />
+                </form>
+        </div>        
+        )
+    }
+
+
 }
 
 export default MoveFunds;
